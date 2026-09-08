f55aa43addd83fecb9083cad85afd90c84245988
shashikumardk <shashikrishna129@gmail.com>
2026-09-08 03:24:19 -0400

    Add JaCoCo coverage gate

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -2,6 +2,9 @@ package com.example;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AppTest {
@@ -13,4 +16,28 @@ class AppTest {
 
         assertEquals(5, result);
     }
+
+    @Test
+    void testMain() {
+
+        PrintStream originalOut = System.out;
+
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+        System.setOut(new PrintStream(output));
+
+        App.main(new String[]{});
+
+        System.setOut(originalOut);
+
+        assertEquals("Hello from Jenkins CI!\n", output.toString());
+    }
+
+    @Test
+    void testAppConstructor() {
+
+        App app = new App();
+
+        assertEquals(App.class, app.getClass());
+    } 
 }
